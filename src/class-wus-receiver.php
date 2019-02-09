bcc7a0a8e71cd1a3cd495c2f388d8fbafa8069f7
patrick_posner <patrick.posner@marketpress.de>
2019-02-09 20:13:44 +0100

    adding import users for post requests

--- src/class-wus-receiver.php
+++ src/class-wus-receiver.php
@@ -19,17 +19,18 @@ class WUS_Receiver {
 	 * Constructor for WUS_Receiver
 	 */
 	public function __construct() {
-		\WP_Route::post( 'api', 'ut\WUS_Receiver::receive_request' );
+		\WP_Route::post( 'wc-user-transfer', 'wus\WUS_Receiver::receive_request' );
 
 	}
 
 	/**
-	 * Listen to $_POST requests on /api/
+	 * Listen to $_POST requests on /wc-user-transfer/
 	 *
 	 * @return void
 	 */
 	public static function receive_request() {
-		// extract data
+		$users = $_POST;
+		self::import_user( $users );
 	}
 
 	/**
@@ -38,8 +39,76 @@ class WUS_Receiver {
 	 * @param array $user_data array with the user data.
 	 * @return void
 	 */
-	public function import_user( $user_data ) {
+	public static function import_user( $users ) {
 
+		$number_of_users = intval( get_option( 'wc_settings_tab_user_synchronisation_batch_users' ) );
+
+		for ( $count = 0; $count < $number_of_users; $count++ ) {
+
+			foreach ( $users as $user ) {
+
+				$old_user = get_user_by( 'email', $user->user_email );
+
+				if ( false === $old_user ) {
+					/*create user and update meta */
+					$userdata = array(
+						'user_login'   => $user['user_login'],
+						'password'     => $user['user_pass'],
+						'nice_name'    => $user['user_nicename'],
+						'user_mail'    => $user['user_email'],
+						'display_name' => $user['display_name'],
+					);
+
+					$user_id = wp_insert_user( $userdata );
+
+					update_user_meta( $user_id, 'billing_first_name', $user['billing_first_name'] );
+					update_user_meta( $user_id, 'billing_last_name', $user['billing_last_name'] );
+					update_user_meta( $user_id, 'billing_company', $user['billing_company'] );
+					update_user_meta( $user_id, 'billing_address_1', $user['billing_address_1'] );
+					update_user_meta( $user_id, 'billing_address_2', $user['billing_address_2'] );
+					update_user_meta( $user_id, 'billing_city', $user['billing_city'] );
+					update_user_meta( $user_id, 'billing_postcode', $user['billing_postcode'] );
+					update_user_meta( $user_id, 'billing_country', $user['billing_country'] );
+					update_user_meta( $user_id, 'billing_state', $user['billing_state'] );
+					update_user_meta( $user_id, 'billing_phone', $user['billing_phone'] );
+					update_user_meta( $user_id, 'billing_email', $user['billing_email'] );
+					update_user_meta( $user_id, 'shipping_first_name', $user['shipping_first_name'] );
+					update_user_meta( $user_id, 'shipping_last_name', $user['shipping_last_name'] );
+					update_user_meta( $user_id, 'shipping_company', $user['shipping_company'] );
+					update_user_meta( $user_id, 'shipping_address_1', $user['shipping_address_1'] );
+					update_user_meta( $user_id, 'shipping_address_2', $user['shipping_address_2'] );
+					update_user_meta( $user_id, 'shipping_city', $user['shipping_city'] );
+					update_user_meta( $user_id, 'shipping_postcode', $user['shipping_postcode'] );
+					update_user_meta( $user_id, 'shipping_country', $user['shipping_country'] );
+					update_user_meta( $user_id, 'shipping_state', $user['shipping_state'] );
+
+				} else {
+					/* update user meta only */
+					$user_id = $old_user->ID;
+
+					update_user_meta( $user_id, 'billing_first_name', $user['billing_first_name'] );
+					update_user_meta( $user_id, 'billing_last_name', $user['billing_last_name'] );
+					update_user_meta( $user_id, 'billing_company', $user['billing_company'] );
+					update_user_meta( $user_id, 'billing_address_1', $user['billing_address_1'] );
+					update_user_meta( $user_id, 'billing_address_2', $user['billing_address_2'] );
+					update_user_meta( $user_id, 'billing_city', $user['billing_city'] );
+					update_user_meta( $user_id, 'billing_postcode', $user['billing_postcode'] );
+					update_user_meta( $user_id, 'billing_country', $user['billing_country'] );
+					update_user_meta( $user_id, 'billing_state', $user['billing_state'] );
+					update_user_meta( $user_id, 'billing_phone', $user['billing_phone'] );
+					update_user_meta( $user_id, 'billing_email', $user['billing_email'] );
+					update_user_meta( $user_id, 'shipping_first_name', $user['shipping_first_name'] );
+					update_user_meta( $user_id, 'shipping_last_name', $user['shipping_last_name'] );
+					update_user_meta( $user_id, 'shipping_company', $user['shipping_company'] );
+					update_user_meta( $user_id, 'shipping_address_1', $user['shipping_address_1'] );
+					update_user_meta( $user_id, 'shipping_address_2', $user['shipping_address_2'] );
+					update_user_meta( $user_id, 'shipping_city', $user['shipping_city'] );
+					update_user_meta( $user_id, 'shipping_postcode', $user['shipping_postcode'] );
+					update_user_meta( $user_id, 'shipping_country', $user['shipping_country'] );
+					update_user_meta( $user_id, 'shipping_state', $user['shipping_state'] );
+				}
+			}
+		}
 	}
 
 }
