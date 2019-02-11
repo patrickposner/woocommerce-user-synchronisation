5d1552c386b18689cc0dbc08b0e677c30ba510b5
patrick_posner <patrick.posner@marketpress.de>
2019-02-11 15:33:37 +0100

    remove WP_Route dependency

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -6,7 +6,7 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    '5f3943c0774896047380645685ad5e10' => $vendorDir . '/anthonybudd/wp_route/src/WP_Route.php',
+    'fe8cbe07f29fde56ff7b5dfd5a18f9f1' => $baseDir . '/src/WP_Route.php',
     '0d077f77ce94f343ff63cacc624c35cf' => $baseDir . '/src/class-wus-admin.php',
     '3121495a34851eaab49be4f1354828ef' => $baseDir . '/src/class-wus-sender.php',
     '87c1b945ab965e4587439b6075ceccbd' => $baseDir . '/src/class-wus-receiver.php',
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -7,7 +7,7 @@ namespace Composer\Autoload;
 class ComposerStaticInitec00dca31fd1f38736e88f1aacf49876
 {
     public static $files = array (
-        '5f3943c0774896047380645685ad5e10' => __DIR__ . '/..' . '/anthonybudd/wp_route/src/WP_Route.php',
+        'fe8cbe07f29fde56ff7b5dfd5a18f9f1' => __DIR__ . '/../..' . '/src/WP_Route.php',
         '0d077f77ce94f343ff63cacc624c35cf' => __DIR__ . '/../..' . '/src/class-wus-admin.php',
         '3121495a34851eaab49be4f1354828ef' => __DIR__ . '/../..' . '/src/class-wus-sender.php',
         '87c1b945ab965e4587439b6075ceccbd' => __DIR__ . '/../..' . '/src/class-wus-receiver.php',
